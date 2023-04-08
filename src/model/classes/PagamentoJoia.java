54561a64185d67adf26e18674442ff9661100559
Luiz Fernando <luizfernando.lf355@gmail.com>
2023-04-08 20:18:53 -0300

    Feat: classe PagamentoJoia implements Pagamento

---
+++ src/model/classes/PagamentoJoia.java
@@ -0,0 +1,37 @@
+package model.classes;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+
+import model.interfaces.Pagamento;
+
+public class PagamentoJoia implements Pagamento {
+
+	private Double valor;
+	private LocalDate data;
+	
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public Double getValor() {
+		return this.valor;
+	}
+
+	@Override
+	public void verPagamento() {
+		DecimalFormat f = new DecimalFormat("#,##");
+		System.out.println("Pagamento dia: " + data + "\t Valor: " + 
+				f.format(valor));
+	}
+
+}
